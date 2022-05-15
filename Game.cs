4515d3216fe4d1e7139c38d8417c669c0e9c06d5
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-15 17:09:30 +0530

    [Raj]Add: The Player reaches the winning position

--- Game.cs
+++ Game.cs
@@ -12,29 +12,38 @@ namespace SnakeandLadder
         {
             int Start = 0, Option = 0;
             Console.Write("\n--------------------- \nSnake and Ladder Game \n"
-                + "---------------------\n" +
-                "Player is at Start position " + Start);
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            Console.WriteLine("\n Player got dice number " + dice);
-
-            Option = random.Next(1, 4);
-            Console.WriteLine("Option = " + Option);
-            switch(Option)
+                 + "---------------------\n" +
+                 "Player is at Start position " + Start);
+            while (Start != 100)
             {
-                case 1:
-                    Console.Write("No Play");
-                    break;
-                case 2:
-                    Console.Write("Ladder: Move up by " + dice);
-                    Start += dice;
-                    break;
-                case 3:
-                    Console.Write("Snake: Move down by " + dice);
-                    break;
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("\n Player got dice number " + dice);
+
+                Option = random.Next(1, 4);
+                Console.WriteLine("Option = " + Option);
+                switch (Option)
+                {
+                    case 1:
+                        Console.Write("No Play");
+                        break;
+                    case 2:
+                        Console.Write("Ladder: Move up by " + dice);
+                        Start += dice;
+                        break;
+                    case 3:
+                        Console.Write("Snake: Move down by " + dice);
+                        Start -= dice;
+                        break;
                     default:
-                    break;
+                        break;
+                }
+                if(Start < 0)
+                {
+                    Start = 0;
+                }
             }
+            Console.Write("\n Player reached at the end of game");
         }
     }
 }
