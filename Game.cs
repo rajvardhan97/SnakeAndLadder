a0788e720a397f31efc7ee7503d9b9f261a7d1e8
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-15 16:37:51 +0530

    [Raj]Add: Single Player at start position

---
+++ Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder
+{
+    internal class Game
+    {
+        public void SnakeLadder()
+        {
+            int Start = 0;
+            Console.Write("\n--------------------- \nSnake and Ladder Game \n"
+                + "---------------------\n" +
+                "Player is at Start position " + Start);
+            
+        }
+    }
+}
